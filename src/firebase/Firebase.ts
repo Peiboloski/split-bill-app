477bfa6224f16c66b2a97706fc0d58d3c59572e5
Pablo D <padelgar@gmail.com>
2024-04-08 12:29:47 +0200

    revert API keys to firebase config

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -1,8 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/functions';
-import { FirebaseConfig } from '../config';
 
-const firebaseConfig = FirebaseConfig;
+//TODO: Deal with exposure of API Keys (expected way with firebase)
+//As firebase need to be executed from frontend, there is a quota and origin added
+const firebaseConfig = {
+    apiKey: "AIzaSyAsJ0Rbs5It01A1RTY61ERO44KO0DjrpqQ",
+    authDomain: "split-bill-app.firebaseapp.com",
+    projectId: "split-bill-app",
+    storageBucket: "split-bill-app.appspot.com",
+    messagingSenderId: "33643061571",
+    appId: "1:33643061571:web:d4526a1cd30774c8cc4ff6",
+};
 
 export default class Firebase {
     public functions: firebase.functions.Functions;
